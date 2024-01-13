f7c2f633b536a12b3734365b3cc08a746f4e0be2
satishraja <satishraja4u@gmail.com>
2024-01-13 14:55:09 +0530

    COMMIT 13012024 -1

---
+++ src/main/java/SatishRaja/DeleteGithubJenkins/App.java
@@ -0,0 +1,13 @@
+package SatishRaja.DeleteGithubJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SatishRaja/DeleteGithubJenkins/GithubJenkinsTest.java
@@ -0,0 +1,14 @@
+package SatishRaja.DeleteGithubJenkins;
+
+import org.testng.annotations.Test;
+
+public class GithubJenkinsTest {
+
+		@Test
+	public void Test01() {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("SatishRaja Eclipse >> GitHub >> Jenkins");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
+	
+}
